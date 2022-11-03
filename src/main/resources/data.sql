4bfd714f756c276a7e79be48bf770b8db2fa9513
shars <sharsir.ss@gmail.com>
2022-11-03 01:42:25 -0500

    Add data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,56 @@
+-- USERS
+
+INSERT INTO app_user (id, name)
+VALUES (1, 'umur inan');
+
+INSERT INTO app_user (id, name)
+VALUES (2, 'john doe');
+
+INSERT INTO app_user (id, name)
+VALUES (3, 'muhyidean l-tarawneh');
+
+INSERT INTO app_user (id, name)
+VALUES (4, 'keith levi');
+
+INSERT INTO app_user (id, name)
+VALUES (5, 'obinna kalu');
+
+INSERT INTO app_user (id, name)
+VALUES (6, 'tina xing');
+
+
+
+-- POSTS
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (1, 'Lizards of Louisiana', 'Yere a lizard larry1', 1);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (2, 'Lizards of Louisiana', 'Yere a lizard larry2', 2);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (3, 'Lizards of Louisiana', 'Yere a lizard larry3', 3);
+
+
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (4, 'Some Awedome title', 'If a body catch a body coming through the rye1', 1);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (5, 'Some Awedome title', 'If a body catch a body coming through the rye2', 2);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (6, 'Some Awedome title', 'If a body catch a body coming through the rye3', 3);
+
+
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (7, 'it is because', 'I yam byatman1', 1);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (8, 'it is because', 'I yam byatman2', 2);
+
+INSERT INTO post (id, title, content, user_id)
+VALUES (9, 'it is because', 'I yam byatman3', 3);
+
+
